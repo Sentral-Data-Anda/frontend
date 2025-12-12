0e6776c347f473208943e183e3e6dc083cd4e233
Alex <lexferndo@Alexs-MacBook-Air.local>
2025-12-12 17:59:35 +0700

    Fix: Save code user local storage

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-  allowedDevOrigins: ["http://192.168.0.102:3000"],
+  allowedDevOrigins: ["http://localhost:3000"],
 
   reactStrictMode: false,
 
